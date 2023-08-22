33db1ef3f2244a1b3413c00f28a43ad3886c7089
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2023-08-22 13:17:31 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 548 ms, Memory: 62384 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,18 @@
+const { log } = require('console');
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+let arr = [];
+let answer = ""
+for(let i=0; i<n; i++){
+    arr.push(input[i+1].split(" ").map(Number));
+}
+arr.sort((a,b) => a[1]-b[1])
+arr.sort((a,b) => a[0]-b[0])
+
+for(let i of arr){
+    answer += i.join(" ")+"\n";
+}
+
+console.log(answer);
