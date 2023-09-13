447bbc28f7a139c0f9eb96795e138313081aebd0
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2023-09-13 10:14:21 +0900

    [Silver II] Title: 병사 배치하기, Time: 180 ms, Memory: 10404 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.js
@@ -0,0 +1,32 @@
+const { log } = require('console');
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+function lowerBound(arr, target, start, end){
+    while(start<end){
+        let mid = parseInt((start+end)/2);
+        if(arr[mid]>=target){
+            end = mid;
+        }
+        else{
+            start = mid+1;
+        }
+    }
+    return end;
+}
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+arr.reverse();
+let arr1 = [0];
+
+for(x of arr){
+    if(arr1[arr1.length-1] < x){
+        arr1.push(x);
+    }
+    else{
+        let index = lowerBound(arr1, x, 0, arr1.length);
+        arr1[index] = x;
+    }
+}
+
+console.log(n-arr1.length+1);
