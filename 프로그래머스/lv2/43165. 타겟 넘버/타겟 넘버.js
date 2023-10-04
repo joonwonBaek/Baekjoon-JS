579db2bcd07094074ccf16a943d0ff6fd95905f1
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2023-10-04 17:15:33 +0900

    [level 2] Title: 타겟 넘버, Time: 1.09 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,15 @@
+function solution(numbers, target) {
+    let answer = 0;
+    function dfs(sum, count) {
+        if(count === numbers.length){
+            if(sum === target) {
+                answer ++;
+            }
+            return
+        }
+        dfs(sum+numbers[count], count+1);
+        dfs(sum-numbers[count], count+1);
+    }
+    dfs(0, 0);
+    return answer;
+}
