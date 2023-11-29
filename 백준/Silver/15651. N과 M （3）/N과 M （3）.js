fd5dae7816f716acf3cef15a2b089eb95314d7b5
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2023-11-29 15:07:58 +0900

    [Silver III] Title: N과 M (3), Time: 2752 ms, Memory: 410600 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.js
@@ -0,0 +1,35 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let [n, m] = input[0].split(" ").map(Number);
+let visited = new Array(n).fill(false);
+let arr = [];
+for (let i = 1; i <= n; i++) {
+  arr.push(i);
+}
+let selected = [];
+
+let answer = "";
+function dfs(arr, depth) {
+  if (depth === m) {
+    let result = [];
+    for (let i of selected) {
+      result.push(arr[i]);
+    }
+    for (let x of result) {
+      answer += x + " ";
+    }
+    answer += "\n";
+    return;
+  }
+
+  for (let i = 0; i < n; i++) {
+    selected.push(i);
+    visited[i] = true;
+    dfs(arr, depth + 1);
+    selected.pop();
+    visited[i] = false;
+  }
+}
+dfs(arr, 0);
+console.log(answer);
