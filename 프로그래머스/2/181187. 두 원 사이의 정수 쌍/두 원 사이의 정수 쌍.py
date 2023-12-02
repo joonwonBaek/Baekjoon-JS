782283be032f04df0cc4b2bd7149efd0a84fbb19
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2023-12-02 22:55:39 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 1632.76 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -0,0 +1,6 @@
+from math import sqrt
+def solution(r1, r2):
+    answer = 0
+    for i in range(r2):
+        answer += int(sqrt(r2**2-i**2))-int(sqrt(max(0, r1**2 - i**2 - 1)))
+    return answer*4
