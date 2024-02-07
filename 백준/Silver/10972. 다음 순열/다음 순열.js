68da05ef0c5dd6a2ac5d0600c42345c23f91b340
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-02-07 23:47:34 +0900

    [Silver III] Title: 다음 순열, Time: 140 ms, Memory: 11184 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "ex.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+
+let i = n - 1;
+while (i > 0 && arr[i] <= arr[i - 1]) i -= 1; // 내림차순이 끝나는 index
+
+if (i === 0) {
+  console.log(-1);
+} else {
+  let j = n - 1;
+  while (arr[j] <= arr[i - 1]) j -= 1;
+  [arr[j], arr[i - 1]] = [arr[i - 1], arr[j]];
+
+  j = n - 1;
+  while (i < j) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+    i += 1;
+    j -= 1;
+  }
+  console.log(arr.join(" "));
+}
