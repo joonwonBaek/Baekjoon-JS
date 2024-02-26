d6a4c0ea1fc2add8208211cd8b8eaba622ba11b6
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-02-26 22:44:43 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "ex.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [A, B, C] = input[0].split(" ").map(Number);
+
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
