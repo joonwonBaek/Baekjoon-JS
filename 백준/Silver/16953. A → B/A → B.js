5048417329fe6d13572ebc79e8c934c786cd47dc
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-04-28 23:47:24 +0900

    [Silver II] Title: A → B, Time: 124 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.js
+++ 백준/Silver/16953. A → B/A → B.js
@@ -1,23 +1,25 @@
-const { log } = require('console');
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "ex.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let [N,K] = input[0].split(" ").map(Number);
-let cnt = 1;
-while(K > N){
-    if(K%2 === 0){
-        K /= 2;
-        cnt +=1;
-    }
-    else {
-        K = (K-1)/10;
-        cnt +=1;
-    }
-}
-if(K === N){
-    console.log(cnt);
-}
-else{
-    console.log(-1);
+[a, b] = input[0].trim().split(" ").map(Number);
+
+let cnt = 0;
+
+while (b > a) {
+  if (b % 10 === 1) {
+    b = Number(String(b).slice(0, String(b).length - 1));
+    cnt += 1;
+  } else if (b % 2 === 0) {
+    b /= 2;
+    cnt += 1;
+  } else {
+    break;
+  }
 }
 
+if (b === a) {
+  console.log(cnt + 1);
+} else {
+  console.log(-1);
+}
