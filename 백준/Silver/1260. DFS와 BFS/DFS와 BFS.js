9f7f35413693621288ee6c52140186e9e549f000
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-04-29 14:17:33 +0900

    [Silver II] Title: DFS와 BFS, Time: 212 ms, Memory: 15628 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -1,49 +1,54 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "ex.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-function DFS(graph, root) { //스택이용
-    const visited = []; //탐색된 노드
-    let willvisit = []; //탐색해야하는 노드
+const [n, m, v] = input[0].split(" ").map(Number);
 
-    willvisit.push(root);
+const d_visited = new Array(n + 1).fill(0);
+const b_visited = new Array(n + 1).fill(0);
+const graph = Array.from({ length: n + 1 }, () => []);
 
-    while(willvisit.length !== 0) {
-        const node = willvisit.shift(); //pop을 사용하고 reverse()를 사용하면 graph가 변함
-        if(!visited.includes(node)) {
-            visited.push(node);
-            willvisit = [...graph[node], ...willvisit]; //스택(LIFO) 최근 들어온게 앞에 들어오고 shift를 하여서 후입 선출되도록
-        }
+for (let i = 1; i <= m; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+  graph[a].push(b);
+  graph[b].push(a);
+}
+
+for (const a of graph) {
+  a.sort((a, b) => a - b);
+}
+
+const dfs_arr = [];
+const dfs = (start) => {
+  if (d_visited[start]) return;
+  d_visited[start] = 1;
+
+  dfs_arr.push(start);
+  for (const i of graph[start]) {
+    if (d_visited[i] === 0) {
+      dfs(i);
     }
-    return visited
-}
+  }
+};
 
-function BFS (graph, root) {
-    const visited = [];
-    let willvisit = [];
-
-    willvisit.push(root);
-
-    while(willvisit.length !== 0) {
-        const node = willvisit.shift();
-        if(!visited.includes(node)) {
-            visited.push(node);
-            willvisit.push(...graph[node]);
-        }
+const bfs_arr = [];
+const bfs = (start) => {
+  let queue = [start];
+  while (queue.length) {
+    let now = queue.shift();
+    if (b_visited[now] === 1) {
+      continue;
     }
-    return visited
-}
-let [n, m, root] = input[0].split(" ").map(Number);
-const graph = Array.from({length: n+1}, () => []);
-
-for(let i = 0; i < m; i++){
-    let [a, b] = input[i+1].split(" ").map(Number);
-    graph[a].push(b);
-    graph[b].push(a);
-}
-
-for(a of graph) {
-    a.sort((a,b) => a-b);
-}
-
-console.log(DFS(graph, root).join(" "));
-console.log(BFS(graph, root).join(" "));
+    b_visited[now] = 1;
+    bfs_arr.push(now);
+    for (const i of graph[now]) {
+      if (b_visited[i] === 0) {
+        queue.push(i);
+      }
+    }
+  }
+};
+dfs(v);
+bfs(v);
+console.log(dfs_arr.join(" "));
+console.log(bfs_arr.join(" "));
