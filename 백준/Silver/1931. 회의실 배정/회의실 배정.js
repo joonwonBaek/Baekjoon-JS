7c567773ae37f0862ddb6c04a9c8aca376721e6c
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-04-29 14:44:36 +0900

    [Silver I] Title: 회의실 배정, Time: 420 ms, Memory: 45580 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -1,26 +1,29 @@
-const { log } = require('console');
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "ex.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let n = Number(input[0]);
-let arr = [];
-for(let i=1; i<=n; i++){
-    arr.push(input[i].split(" ").map(Number));
+const n = Number(input[0]);
+let end = 0;
+let ans = 0;
+
+const arr = [];
+for (let i = 1; i <= n; i++) {
+  const data = input[i].split(" ").map(Number);
+  arr.push(data);
 }
-arr.sort((a,b)=> {
-    if(a[1]!=b[1]){
-        return a[1]-b[1];
-    }
-    else{
-        return a[0]-b[0];
-    }
-})
-let cnt = 1;
-let cur = 0;
-for(let i=1; i<arr.length; i++){
-    if(arr[cur][1] <= arr[i][0]){
-        cnt += 1;
-        cur = i;
-    }
+arr.sort((a, b) => {
+  if (a[1] !== b[1]) {
+    return a[1] - b[1];
+  } else {
+    return a[0] - b[0];
+  }
+});
+
+for (const a of arr) {
+  if (a[0] >= end) {
+    end = a[1];
+    ans += 1;
+  }
 }
-console.log(cnt);
+
+console.log(ans);
