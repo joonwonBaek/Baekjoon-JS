38c61492f41add43addf206902dedc059554bdd0
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-05-03 14:36:50 +0900

    [level 2] Title: 땅따먹기, Time: 201.68 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -0,0 +1,10 @@
+def solution(land):
+    answer = 0
+    n = len(land)
+    dp = land
+    
+    for i in range(1, n):
+        for j in range(4):
+            dp[i][j] += max(dp[i-1][:j]+dp[i-1][j+1:])
+        
+    return max(dp[n-1])
