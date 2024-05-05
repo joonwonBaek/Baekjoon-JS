12366d161825fc0e46c497a6ddc0257952dbb4fa
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-05-05 22:54:15 +0900

    [Gold V] Title: 회문, Time: 184 ms, Memory: 31308 KB -BaekjoonHub

---
+++ 백준/Gold/17609. 회문/회문.py
@@ -0,0 +1,32 @@
+t = int(input())
+
+for _ in range(t):
+    answer = 2
+    arr = input()
+    start = 0
+    end = len(arr)-1
+    if arr == arr[::-1]:
+        answer = 0
+    else:
+        while start <= end:
+            if arr[start] == arr[end]:
+                start += 1
+                end -= 1
+            else:
+                if end - start == 1:
+                    temp = arr[:start] + arr[end:]
+                    if temp == temp[::-1]:
+                        answer = 1
+                        break
+                if start < end - 1:
+                    temp = arr[:end] + arr[end+1:]
+                    if temp == temp[::-1]:
+                        answer = 1
+                        break
+                if start + 1 < end:
+                    temp = arr[:start] + arr[start+1:]
+                    if temp == temp[::-1]:
+                        answer = 1
+                        break
+                break
+    print(answer)
