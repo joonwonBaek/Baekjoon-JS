f911924560f984414aa0eed6f6efae2bac488c18
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-05-24 00:39:25 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 208 ms, Memory: 11016 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
@@ -1,46 +1,45 @@
-const { log } = require("console");
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+const readline = require("readline");
 
-let n = Number(input[0]);
-let num = input[1].split(" ").map(Number);
-let operator = input[2].split(" ").map(Number);
+// 입출력을 위한 인터페이스 객체 생성
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-let max = Number.MIN_SAFE_INTEGER;
-let min = Number.MAX_SAFE_INTEGER;
+const input = [];
 
-const calculator = ["+", "-", "*", "/"];
-
-const isCalculate = (n, a, b) => {
-  switch (n) {
-    case "+":
-      return a + b;
-    case "-":
-      return a - b;
-    case "*":
-      return a * b;
-    case "/":
-      return parseInt(a / b);
+rl.on("line", (line) => {
+  input.push(line);
+  if (input.length === 3) {
+    rl.close();
   }
-};
-
-const dfs = (count, result) => {
-  if (count === n - 1) {
-    max = Math.max(max, result);
-    min = Math.min(min, result);
-  } else {
-    for (let i = 0; i < operator.length; i++) {
-      if (operator[i] === 0) {
-        continue;
-      }
-      operator[i]--;
-      dfs(count + 1, isCalculate(calculator[i], result, num[count + 1]));
-      operator[i]++;
+}).on("close", () => {
+  const n = Number(input[0]);
+  const num = input[1].split(" ").map(Number);
+  const oper = input[2].split(" ").map(Number);
+  const answer = [];
+  const dfs = (ans, idx, plus, minus, mul, div) => {
+    if (idx === n) {
+      answer.push(ans);
+      return;
     }
-  }
-};
 
-dfs(0, num[0]);
+    if (plus > 0) {
+      dfs(ans + num[idx], idx + 1, plus - 1, minus, mul, div);
+    }
+    if (minus > 0) {
+      dfs(ans - num[idx], idx + 1, plus, minus - 1, mul, div);
+    }
+    if (mul > 0) {
+      dfs(ans * num[idx], idx + 1, plus, minus, mul - 1, div);
+    }
+    if (div > 0) {
+      dfs(~~(ans / num[idx]), idx + 1, plus, minus, mul, div - 1);
+    }
+  };
+  dfs(num[0], 1, oper[0], oper[1], oper[2], oper[3]);
 
-console.log(max ? max : 0);
-console.log(min ? min : 0);
+  console.log(Math.max(...answer));
+  console.log(Math.min(...answer));
+  process.exit();
+});
