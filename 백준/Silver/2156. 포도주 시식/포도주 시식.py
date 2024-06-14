ca4885873e4fd2b094335005584d4b335f69402b
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-06-14 23:38:32 +0900

    [Silver I] Title: 포도주 시식, Time: 576 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+dp = [0] * n
+
+if n >= 1:
+    dp[0] = arr[0]
+if n >= 2:
+    dp[1] = arr[0] + arr[1]
+if n >= 3:
+    dp[2] = max(arr[0] + arr[2], arr[1] + arr[2], dp[1])
+
+for i in range(3, n):
+    dp[i] = max(dp[i-1], dp[i-2] + arr[i], dp[i-3] + arr[i] + arr[i-1])
+
+print(max(dp))
