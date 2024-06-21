05226eb9ef706908830dbe4af3440c12df6dd189
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-06-21 13:32:27 +0900

    [Silver I] Title: 볼 모으기, Time: 284 ms, Memory: 38404 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.js
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.js
@@ -4,10 +4,13 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const n = Number(input[0]);
 const arr = input[1].split("");
+
+// 오른쪽으로 빨간, 파랑 넘기기
 let red = 0;
 let blue = 0;
-const visited_r = new Array(n).fill(0);
-const visited_b = new Array(n).fill(0);
+let visited_r = new Array(n).fill(0);
+let visited_b = new Array(n).fill(0);
+let answer = [];
 
 for (let i = n - 2; i > -1; i--) {
   if (arr[i] == "R") {
@@ -36,6 +39,44 @@ for (let i = n - 2; i > -1; i--) {
     }
   }
 }
+answer.push(red);
+answer.push(blue);
 
-let answer = Math.min(red, blue);
-console.log(answer);
+// 왼쪽으로 빨강, 파랑 넘기기
+red = 0;
+blue = 0;
+visited_r = new Array(n).fill(0);
+visited_b = new Array(n).fill(0);
+
+for (let i = 1; i < n; i++) {
+  if (arr[i] == "R") {
+    if (arr[i - 1] == "B") {
+      visited_r[i] = 1;
+      red += 1;
+    } else {
+      if (visited_r[i - 1] == 1) {
+        visited_r[i] = 1;
+        red += 1;
+      }
+    }
+  }
+}
+
+for (let i = 1; i < n; i++) {
+  if (arr[i] == "B") {
+    if (arr[i - 1] == "R") {
+      visited_b[i] = 1;
+      blue += 1;
+    } else {
+      if (visited_b[i - 1] == 1) {
+        visited_b[i] = 1;
+        blue += 1;
+      }
+    }
+  }
+}
+
+answer.push(red);
+answer.push(blue);
+
+console.log(Math.min(...answer));
