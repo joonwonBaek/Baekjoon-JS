cab8133cebff431be7e71cfdbd31e01cc053d556
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-06-21 13:22:20 +0900

    [Silver I] Title: 볼 모으기, Time: 148 ms, Memory: 9324 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "ex.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const n = Number(input[0]);
+const arr = input[1].split("");
+let red = 0;
+let blue = 0;
+const visited_r = new Array(n).fill(0);
+const visited_b = new Array(n).fill(0);
+
+for (let i = n - 2; i > -1; i--) {
+  if (arr[i] == "R") {
+    if (arr[i + 1] == "B") {
+      visited_r[i] = 1;
+      red += 1;
+    } else {
+      if (visited_r[i + 1] == 1) {
+        visited_r[i] = 1;
+        red += 1;
+      }
+    }
+  }
+}
+
+for (let i = n - 2; i > -1; i--) {
+  if (arr[i] == "B") {
+    if (arr[i + 1] == "R") {
+      visited_b[i] = 1;
+      blue += 1;
+    } else {
+      if (visited_b[i + 1] == 1) {
+        visited_b[i] = 1;
+        blue += 1;
+      }
+    }
+  }
+}
+
+let answer = Math.min(red, blue);
+console.log(answer);
