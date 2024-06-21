9c0295d183f0efd9cedf18654b605a6eb62a9367
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-06-21 14:32:31 +0900

    [Silver II] Title: 부분수열의 합, Time: 188 ms, Memory: 11136 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
@@ -3,17 +3,17 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "ex.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [n, s] = input[0].split(" ").map(Number);
-
 const arr = input[1].split(" ").map(Number);
+let answer = 0;
 let ans = [];
-let cnt = 0;
+
 const dfs = (start) => {
-  const sum = ans.reduce((a, c) => {
+  let temp = ans.reduce((a, c) => {
     return a + c;
   }, 0);
 
-  if (sum === s && ans.length > 0) {
-    cnt += 1;
+  if (temp === s && ans.length > 0) {
+    answer += 1;
   }
 
   for (let i = start; i < n; i++) {
@@ -22,7 +22,6 @@ const dfs = (start) => {
     ans.pop();
   }
 };
-
 dfs(0);
 
-console.log(cnt);
+console.log(answer);
