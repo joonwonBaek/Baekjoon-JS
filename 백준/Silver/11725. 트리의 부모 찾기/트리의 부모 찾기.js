b12a7b055746b68887be23f5de4476e584b82fa3
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-06-25 00:45:01 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 632 ms, Memory: 101292 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
@@ -3,20 +3,19 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "ex.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const n = Number(input[0]);
-
-const visited = new Array(n + 1).fill(0);
-const graph = Array.from({ length: n + 1 }, () => []);
+const tree = Array.from({ length: n + 1 }, () => []);
 const answer = new Array(n + 1).fill(0);
-for (i = 1; i < n; i++) {
+const visited = new Array(n + 1).fill(0);
+
+for (let i = 1; i < n; i++) {
   const [a, b] = input[i].split(" ").map(Number);
-  graph[a].push(b);
-  graph[b].push(a);
+  tree[a].push(b);
+  tree[b].push(a);
 }
 
 const dfs = (start) => {
   visited[start] = 1;
-
-  for (const now of graph[start]) {
+  for (const now of tree[start]) {
     if (visited[now] == 0) {
       answer[now] = start;
       dfs(now);
@@ -25,10 +24,5 @@ const dfs = (start) => {
 };
 
 dfs(1);
-
-let result = "";
-for (let i = 2; i < answer.length; i++) {
-  result += answer[i] + "\n";
-}
-
-console.log(result);
+const temp = answer.slice(2).join("\n");
+console.log(temp);
