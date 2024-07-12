f453da7354c10d7a7ecc1f363b748dc717d7aa25
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-07-12 16:27:19 +0900

    [level 2] Title: [3차] n진수 게임, Time: 4.76 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -0,0 +1,12 @@
+function solution(n, t, m, p) {
+    let answer = '';
+    let candidate = '';
+    
+    for(let i = 0; i < t*m; i++){
+        candidate += i.toString(n).toUpperCase()
+    }
+    for(let j = p-1; j < t*m; j+=m){
+        answer += candidate[j]
+    }
+    return answer;
+}
