a42d71da95c27dcfee90f037715e1307b1e8a5d7
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-09-09 22:07:16 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1824 ms, Memory: 200660 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "ex.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [n, m, k, x] = input[0].split(" ").map(Number);
+
+const graph = Array.from({ length: n + 1 }, () => []);
+
+const visited = Array.from({ length: n + 1 }, () => 0);
+
+const answer = [];
+
+for (let i = 1; i <= m; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+  graph[a].push(b);
+}
+
+const bfs = (start) => {
+  const queue = [start];
+  visited[start] = 1;
+
+  while (queue.length) {
+    const v = queue.shift();
+    if (visited[v] === k + 1) {
+      answer.push(v);
+      continue;
+    }
+
+    for (const next of graph[v]) {
+      if (!visited[next]) {
+        queue.push(next);
+        visited[next] = visited[v] + 1;
+      }
+    }
+  }
+};
+
+bfs(x);
+
+answer.sort((a, b) => a - b);
+
+if (answer.length === 0) {
+  console.log(-1);
+} else {
+  for (const a of answer) {
+    console.log(a);
+  }
+}
