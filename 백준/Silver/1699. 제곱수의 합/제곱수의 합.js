1af41de73b077e5e1eb4cba15049f1b85e7e4a86
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-10-04 22:45:45 +0900

    [Silver II] Title: 제곱수의 합, Time: 280 ms, Memory: 11784 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "ex.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const n = Number(input[0]);
+
+const dp = Array(n + 1).fill(Infinity);
+
+dp[0] = 0;
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j * j <= i; j++) {
+    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+  }
+}
+
+console.log(dp[n]);
