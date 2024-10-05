f7ebc23c3725c957a3b88662eebb604b552d8dc9
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-10-05 20:24:51 +0900

    [Silver II] Title: 상자넣기, Time: 152 ms, Memory: 9668 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "ex.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const n = Number(input[0]);
+const arr = input[1].split(" ").map(Number);
+
+const dp = Array(n).fill(1);
+
+for (let i = 1; i < n; i++) {
+  for (let j = 0; j < i; j++) {
+    if (arr[j] < arr[i]) {
+      dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+  }
+}
+
+console.log(Math.max(...dp));
