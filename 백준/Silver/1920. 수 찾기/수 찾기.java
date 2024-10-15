508498e2fad87ad58d5c8cf8f20adf333bc8dd8a
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-10-15 15:52:13 +0900

    [Silver IV] Title: 수 찾기, Time: 584 ms, Memory: 42620 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+        int []arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            int cnt = Arrays.binarySearch(arr, num);
+            if(cnt < 0 ) {
+                bw.write(0 + "\n");
+            } else {
+                bw.write(1 + "\n");
+            }
+            
+        };
+
+        bw.flush();
+        bw.close();
+    }
+}
