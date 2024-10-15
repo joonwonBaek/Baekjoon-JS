94796d4e5d0441f83381f862b1b6f9bb4387ac4a
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-10-15 22:03:07 +0900

    [Silver III] Title: 소수 구하기, Time: 468 ms, Memory: 20876 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        for(int i = m; i<=n; i++) {
+            if(findNumber(i)) {
+                bw.write(i + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    private static boolean findNumber(int number) {
+        if(number < 2) {
+            return false;
+        }
+        for(int i = 2; i <= Math.sqrt(number); i++) {
+            if(number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
