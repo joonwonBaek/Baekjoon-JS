9936ead169056d3c9106404e69e190914371bff8
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-10-16 14:39:00 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 19760 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n+1];
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + 1;
+            if (i % 2 == 0) arr[i] = Math.min(arr[i], arr[i / 2] + 1);
+            if (i % 3 == 0) arr[i] = Math.min(arr[i], arr[i / 3] + 1);
+        }
+        bw.write(arr[n] + "\n"); 
+        bw.flush();
+        bw.close();
+    }
+}
