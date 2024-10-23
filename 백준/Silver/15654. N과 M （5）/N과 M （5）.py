92e3ddd928a47df75c887972a96fcd469f4ab8f1
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-10-23 23:59:57 +0900

    [Silver III] Title: N과 M (5), Time: 100 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -2,18 +2,22 @@ n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
 arr.sort()
+answer = ''
+visited = [0] * n
 
-def dfs(depth):
+def dfs(depth, total):
+    global answer
     if depth == m:
-        print(' '.join(map(str, box)))
+        print(total)
         return
 
     for i in range(n):
-        if arr[i] in box:
-            continue
-        box.append(arr[i])
-        dfs(depth + 1)
-        box.pop()
+        if visited[i] == 0:
+            visited[i] = 1
+            dfs(depth + 1, total + " " + str(arr[i]))
+            visited[i] = 0
 
-box = []
-dfs(0)
+for i in range(n):
+    visited[i] = 1
+    dfs(1, str(arr[i]))
+    visited[i] = 0
