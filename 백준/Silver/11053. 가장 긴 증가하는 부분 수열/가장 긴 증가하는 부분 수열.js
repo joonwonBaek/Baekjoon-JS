de2ca61ba03172af11c79c24dae5f69affbd3968
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-10-23 23:30:31 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 188 ms, Memory: 9676 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.js
@@ -1,21 +1,17 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "ex.txt";
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const { log } = require("console");
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-const n = Number(input[0]);
+let n = Number(input[0]);
 
-const arr = input[1].split(" ").map(Number);
+let arr = input[1].split(" ").map(Number);
 
-const dp = new Array(n).fill(0);
+dp = new Array(n).fill(1);
 
-dp[0] = 1;
-
-for (let i = 1; i < n; i++) {
+for (let i = 0; i < n; i++) {
   for (let j = 0; j < i; j++) {
     if (arr[i] > arr[j]) {
       dp[i] = Math.max(dp[i], dp[j] + 1);
-    } else {
-      dp[i] = Math.max(dp[i], 1);
     }
   }
 }
