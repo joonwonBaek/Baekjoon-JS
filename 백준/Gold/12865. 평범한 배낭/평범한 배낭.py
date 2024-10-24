eaab662492bf783a55c19dd564b808605916d35c
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-10-24 17:12:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 4856 ms, Memory: 229084 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,15 +1,15 @@
 n, k = map(int, input().split())
-
-bag = [list(map(int, input().split())) for _ in range(n)]
+bags = []
+for _ in range(n):
+    bags.append(list(map(int, input().split())))
 
 dp = [[0]*(k+1) for _ in range(n+1)]
 
 for i in range(1, n+1):
-    w, v = bag[i-1]
+    w, v = bags[i-1]
     for j in range(1, k+1):
         if j >= w:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v)
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
         else:
             dp[i][j] = dp[i-1][j]
-
-print(dp[-1][-1])
+print(dp[n][k])
