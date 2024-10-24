e78ac13fdbfcdfe83d9379a3941384dace26a531
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-10-24 13:44:41 +0900

    [Silver I] Title: 스티커, Time: 940 ms, Memory: 49108 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -19,7 +19,7 @@ for _ in range(t):
         continue
 
     for i in range(2, n):
-        dp[0][i] = max(dp[1][i-2], dp[1][i-1]) + arr[0][i]
-        dp[1][i] = max(dp[0][i-2], dp[0][i-1]) + arr[1][i]
+        dp[0][i] = max(dp[1][i-1], dp[1][i-2], dp[0][i-2]) + arr[0][i]
+        dp[1][i] = max(dp[0][i-1], dp[0][i-2], dp[1][i-2]) + arr[1][i]
 
-    print(max(dp[0][-1], dp[1][-1]))
+    print(max(dp[0][n-1], dp[1][n-1]))
