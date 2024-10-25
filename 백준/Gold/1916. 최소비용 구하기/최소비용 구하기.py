be5ef3ff1a9bee86aa69a5fcd2a0a52bb6592a9c
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-10-25 13:41:41 +0900

    [Gold V] Title: 최소비용 구하기, Time: 380 ms, Memory: 60444 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,27 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b, cost = map(int, input().split())
+    graph[a].append([b, cost])
+
+start, end = map(int, input().split())
+costs = [int(1e9)] * (n+1)
+heap = []
+heapq.heappush(heap, [0, start])
+
+while heap:
+    cur_cost, cur_v = heapq.heappop(heap)
+    if costs[cur_v] < cur_cost:
+        continue
+    for next_v, next_cost in graph[cur_v]:
+        sum_cost = cur_cost + next_cost
+        if sum_cost < costs[next_v]:
+            costs[next_v] = sum_cost
+            heapq.heappush(heap, [sum_cost, next_v])
+print(costs[end])
