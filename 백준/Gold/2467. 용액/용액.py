87dc86f559cbd2a35403c4a1a9141e7467320ab3
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-10-28 16:25:50 +0900

    [Gold V] Title: 용액, Time: 120 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -2,21 +2,24 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 left = 0
-right = n-1
-ans = abs(arr[left] + arr[right])
-result = []
+right = n - 1
+answer = abs(arr[left] + arr[right])
+ans_left = left
+ans_right = right
 
 while left < right:
     temp = arr[left] + arr[right]
-    if abs(temp) <= ans:
-        ans = abs(temp)
-        result = [arr[left], arr[right]]
+    if abs(temp) < answer:
+        ans_left = left
+        ans_right = right
+        answer = abs(temp)
 
-        if ans == 0:
+        if answer == 0:
             break
 
     if temp < 0:
         left += 1
     else:
         right -= 1
-print(*result)
+
+print(arr[ans_left], arr[ans_right])
