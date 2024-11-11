91e6edce9fc23616d3d0080d89e8fb172700e58c
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-11-11 17:55:54 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 3268 ms, Memory: 77304 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,28 +1,29 @@
 import heapq
 
 v, e = map(int, input().split())
-visited = [0]*(v+1)
-heap = []
 
+visited = [0] * (v+1)
 graph = [[] for _ in range(v+1)]
+
 for _ in range(e):
     a, b, c = map(int, input().split())
     graph[a].append([c, b])
     graph[b].append([c, a])
 
 def prim(start):
+    heap = []
     heapq.heappush(heap, [0, start])
-    weight = 0
-    while heap:
-        w, node = heapq.heappop(heap)
+    answer = 0
+    cnt = 0
 
-        if visited[node] == 0:
-            visited[node] = 1
-            weight += w
-            for edge in graph[node]:
-                if visited[edge[1]] == 0:
-                    heapq.heappush(heap, edge)
-    return weight
+    while cnt < v:
+        n_cost, n_node = heapq.heappop(heap)
+        if visited[n_node]: continue
+        visited[n_node] = 1
+        answer += n_cost
+        cnt += 1
+        for next_cost, next_node in graph[n_node]:
+            heapq.heappush(heap, [next_cost, next_node])
+    return answer
 
 print(prim(1))
-
