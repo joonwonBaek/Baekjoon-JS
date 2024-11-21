e5fb86c416f93a3ac6633c882a2da5624865d585
joonwonBaek <44563138+joonwonBaek@users.noreply.github.com>
2024-11-21 15:36:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 3872 ms, Memory: 20808 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -2,25 +2,26 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "ex.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const [n, k] = input[0].split(" ").map(Number);
+let [n, k] = input[0].split(" ").map(Number);
 
-const bfs = (cnt) => {
-  let queue = [n];
-  let visited = Array.from({ length: 100001 }, () => 0);
+const visited = Array(100001).fill(0);
+let cnt = 0;
+
+const bfs = () => {
+  const q = [];
+  q.push([n, 0]);
   visited[n] = 1;
 
-  while (queue.length) {
-    let x = queue.shift();
-    if (x === k) {
-      return visited[x] - 1;
-    }
-    for (let nx of [x - 1, x + 1, x * 2]) {
-      if (nx >= 0 && nx <= 100000 && !visited[nx]) {
-        visited[nx] = visited[x] + 1;
-        queue.push(nx);
+  while (q.length) {
+    const [now, cnt] = q.shift();
+    if (now === k) return cnt;
+    for (const nx of [now - 1, now + 1, now * 2]) {
+      if (nx >= 0 && nx <= 100000 && visited[nx] === 0) {
+        visited[nx] = 1;
+        q.push([nx, cnt + 1]);
       }
     }
   }
 };
 
-console.log(bfs(0));
+console.log(bfs());
